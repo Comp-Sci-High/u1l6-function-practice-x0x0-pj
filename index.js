2b49e0508064a7e1c7d43c1b37abbecd99767a1d
x0x0-pj <pryah.statom26@compscihigh.org>
2025-09-17 16:40:18 +0000

    all tasks complete

--- index.js
+++ index.js
@@ -12,26 +12,47 @@
 // 2. Inside the function, return the result of dividing num1 by num2.
 // 3. Call the `divide` function with the inputs 4 and 5, and log the result.
 
+function divide(num1,num2) {
+   return num2/num1;
+}
 
+let result = divide(4,5)
 
 // TASK 2: Define a regular function to greet a user.
 // 1. Define a function named `greetUser` that takes one input: name.
 // 2. Inside the function, return a greeting string like "Hello, [name]!"
 // 3. Call the `greetUser` function with your name, and log the result.
 
+let name = PromiseRejectionEvent
 
+function greetUser(name){
+   return "Hello, [name]!";
+
+}
+
+let output = greetUser
 
 // TASK 3: Define an arrow function to subtract two numbers.
 // 1. Define an arrow function named `subtract` that takes two inputs: num1 and num2.
 // 2. Inside the arrow function, return the result of subtracting num2 from num1.
 // 3. Call the `subtract` function with the inputs 10 and 3, and log the result.
 
+const subtract = (num1,num2) => {
+   return num1 - num2;
+}
 
+let diff = subtract(10,3)
 
 // TASK 4: Define an arrow function to calculate the square of a number.
 // 1. Define an arrow function named `square` that takes one input: num.
 // 2. Inside the arrow function, return the result of squaring the number (num * num).
 // 3. Call the `square` function with the argument 6, and log the result.
+
+const spuare = (num) => {
+   return num * num
+};
+
+let sq = square
 
 /*
    ==== Final Commit Instructions ====
